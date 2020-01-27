77f8a878361dc65eed6c8cbbde99ddcd8fd4862e
Harmon5919 <57842300+Harmon5919@users.noreply.github.com>
2020-01-27 13:13:04 -0700

    Version 2

--- GradesManager1/Program.cs
+++ GradesManager1/Program.cs
@@ -8,57 +8,76 @@ namespace GradesManager1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Please select from the options below" + "\n" + "Main Menu:" + "\n" + "1. Enter Student Info" + "\n" + "2. Display grade average" + "\n" + "3. Quit");
-            bool quit = false;
-            string select = Console.ReadLine();
-            while (quit == false)
+            bool quit = false; //if false, program keeps cycling, if true, program ends
+            List<string> Students = new List<string>(); //string list to store names of students
+            List<decimal> Grades = new List<decimal>(); //decimal list to store grades of students
+            decimal average = 0;
+
+            while (quit == false) //keeps iterating until user selects option 3
             {
-                if (select == "1")
+                Console.WriteLine("Please select from the options below" + "\n" + "Main Menu:" + "\n" + "1. Enter Student Info" + "\n" + "2. Display grade average" + "\n" + "3. Quit");
+                string select = Console.ReadLine(); //variable to store user input for menu options 1, 2, or 3
+                if (select == "1") //if the user selects "1. Enter Student Info"
                 {
-                    StudentMenu();
+                    Console.WriteLine("Please type the name of a student.");
+                    string name = Console.ReadLine(); //user types a name to add it to the Students list
+                    Students.Add(name); //^
+                    bool selection = true;
+                    Console.WriteLine(name + " has been added to the list. Please enter a grade.");
+                    decimal grade = decimal.Parse(Console.ReadLine());
+                    int count = 0;
+                    average += grade;
+                    while (selection == true)
+                    {
+ 
+                        Console.WriteLine("Would you like to add another grade?");
+                        string response = Console.ReadLine();
+                        if (response.ToLower() == "yes")
+                        {
+                            grade = decimal.Parse(Console.ReadLine());
+                            average += grade;
+                            count++;
+                        }
+                        else
+                        {
+                            selection = false;
+                        }
+                    }
+                    average = average / count;
+                    Grades.Add(average);
+                }
+                else if (select =="2") //if user selects option 2. Display Grade Average
+                {
+                    //displays contents of Students list
+                    Console.WriteLine("The current students are:"); 
+                    for (int i =0; i < Students.Count; i++)
+                    {
+                        Console.WriteLine(Students.ElementAt(i));
+                    }
+                    Console.WriteLine("Please type the name of a student above to calculate their average grade.");
+                    string match = Console.ReadLine(); //user types name of any student displayed above
+                    for (int i = 0; i < Students.Count; i++)
+                    {
+                        if (match == Students.ElementAt(i))
+                        {
+                            Console.WriteLine(Students.ElementAt(i) + "'s average grade is " + Grades.ElementAt(i));
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine(match + "was not found. Please enter a name from the list above"); //if typed name does not match name in list
+                        }
+                    }
+                }
+                else if (select == "3")
+                {
+                    quit = true;
                 }
             }
             Console.WriteLine("Thank you. Goodbye.");
             Console.ReadKey();
-        }
-        static void StudentMenu()
-        {
-            List<string> Students = new List<string>();
-            List<decimal> Grades = new List<decimal>();
-
-            /*for (int i = 0; i < Students.Count; i++)
-            {
-                Console.WriteLine(Students.ElementAt(i));
-            }*/
-            Console.WriteLine("Please type the name of a student.");
-            string name = Console.ReadLine();
-            bool quit = false;
-            while (quit == false)
-            {
-                Console.WriteLine("You are viewing the file for " + name + "\n" + "1. Enter a new grade" + "\n" + "2. Quit");
-                string selection = Console.ReadLine();
-                if (selection == "2")
-                {
-                    quit = true;
-                }
-                else
-                {
-                    Console.WriteLine("Please enter a new grade");
-                    decimal grade = decimal.Parse(Console.ReadLine());
-                    Grades.Add(grade);
-                }
-            }
-            else
-            {
-                Students.Add(name);
-            }
-            }
-        }
-        public static void GradeMenu()
-        {
-           
         }
     }
 }
