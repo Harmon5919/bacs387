f5d6175d8e59329f0a659fb4c8021273f96a3a67
Harmon5919 <57842300+Harmon5919@users.noreply.github.com>
2020-01-29 09:07:05 -0700

    First Start

---
+++ GradeManager2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeManager2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> StudID = new List<string>();
+            List<string> First = new List<string>();
+            List<string> Last = new List<string>();
+
+            Console.WriteLine("Main Menu:" + "\n" + "1. Enter Student Info" + "\n" + "2. Enter Grades" + "\n" + "3. Remove Students" + "\n" + "4. Grade Analytics");
+        }
+    }
+}
