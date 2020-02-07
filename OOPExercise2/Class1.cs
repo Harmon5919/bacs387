aa5887ff122ea834542ff3ba87002e8a89870866
Harmon5919 <57842300+Harmon5919@users.noreply.github.com>
2020-02-07 10:28:25 -0700

    copy classes to oop2

---
+++ OOPExercise2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExercise1
+{
+    class Student
+    {
+        public Student(string firstName, string lastName, decimal grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Grade { get; set; }
+    }
+}
---
+++ OOPExercise2/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExercise1
+{
+    class Course
+    {
+        public Course(string courseName, int courseId)
+        {
+            this.CourseName = courseName;
+            this.CourseId = courseId;
+        }
+        public string CourseName { get; set; }
+        public int CourseId { get; set; }
+
+    }
+}
