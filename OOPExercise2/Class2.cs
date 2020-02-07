acd2cc0e224fcf95596a6bb7810434082da64603
Harmon5919 <57842300+Harmon5919@users.noreply.github.com>
2020-02-07 10:57:04 -0700

    begin oop2

--- OOPExercise2/Class2.cs
+++ OOPExercise2/Class2.cs
@@ -4,17 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOPExercise1
+namespace OOPExercise2
 {
     class Course
     {
-        public Course(string courseName, int courseId)
+        public Course(string courseName, int courseId, List<Student> studentList)
         {
             this.CourseName = courseName;
             this.CourseId = courseId;
+            this.StudentList = studentList;
         }
         public string CourseName { get; set; }
         public int CourseId { get; set; }
+        public List<Student> StudentList { get; set; }
 
+        public void GetAverageGrade()
+        {
+            decimal average = 0;
+
+        }
+
+        public void GetMin()
+        {
+            int min = 100;
+        }
+
+        public void GetMax()
+        {
+
+        }
+
+        public void GetPercent()
+        {
+
+        }
     }
 }
--- OOPExercise2/Program.cs
+++ OOPExercise2/Program.cs
@@ -10,6 +10,20 @@ namespace OOPExercise2
     {
         static void Main(string[] args)
         {
+            Student myself = new Student("Hanna", "Harmon", 90);
+            List<Student> spring = new List<Student>();
+            spring.Add(new Student("Hannah", "Harmon", 90));
+            spring.Add(new Student("Moe", "Manshad", 100));
+            spring.Add(new Student("John", "Smith", 0));
+            decimal average = 0;
+            for (int i = 0; i < spring.Count; i++)
+            {
+                Console.WriteLine(spring[i].FirstName + " " + spring[i].LastName + " " + spring[i].Grade);
+                average += spring[i].Grade;
+            }
+            average = average / spring.Count;
+            Console.WriteLine("Class average is " + average);
+            Console.ReadKey();
         }
     }
 }
