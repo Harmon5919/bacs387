17a065306c02f889d7534f6b42d751a853ab7c79
Harmon5919 <57842300+Harmon5919@users.noreply.github.com>
2020-02-07 09:57:35 -0700

    network client app

---
+++ BACS380UDPClient/BACS380UDPClient/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace BACS380UDPClient
+{
+    partial class frmClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtIPadd = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblIPadd = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.BackColor = System.Drawing.Color.Linen;
+            this.txtMessage.Location = new System.Drawing.Point(142, 65);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(404, 20);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // txtIPadd
+            // 
+            this.txtIPadd.BackColor = System.Drawing.Color.Linen;
+            this.txtIPadd.Location = new System.Drawing.Point(142, 115);
+            this.txtIPadd.Name = "txtIPadd";
+            this.txtIPadd.Size = new System.Drawing.Size(194, 20);
+            this.txtIPadd.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.BackColor = System.Drawing.Color.Linen;
+            this.txtPort.Location = new System.Drawing.Point(142, 165);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(75, 20);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "1100";
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.Linen;
+            this.btnSend.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(261, 233);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 28);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(65, 68);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(58, 16);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "Message:";
+            // 
+            // lblIPadd
+            // 
+            this.lblIPadd.AutoSize = true;
+            this.lblIPadd.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIPadd.Location = new System.Drawing.Point(58, 116);
+            this.lblIPadd.Name = "lblIPadd";
+            this.lblIPadd.Size = new System.Drawing.Size(69, 16);
+            this.lblIPadd.TabIndex = 5;
+            this.lblIPadd.Text = "IP Address:";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPort.Location = new System.Drawing.Point(58, 166);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(78, 16);
+            this.lblPort.TabIndex = 6;
+            this.lblPort.Text = "Port Number:";
+            // 
+            // frmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(623, 317);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblIPadd);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIPadd);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "frmClient";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtIPadd;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblIPadd;
+        private System.Windows.Forms.Label lblPort;
+    }
+}
+
---
+++ BACS380UDPClient/BACS380UDPClient/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets; //added
+
+namespace BACS380UDPClient
+{
+    public partial class frmClient : Form
+    {
+        public frmClient()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            UdpClient udpClient = new UdpClient(1100); //creates new instance of class using constructor of same name as class, parameter of port number
+            Byte[] sendBytes = new Byte[] { }; //creates container for bytes
+            sendBytes = Encoding.ASCII.GetBytes(txtMessage.Text); //puts text from textbox into array of bytes using library
+            udpClient.Send(sendBytes, sendBytes.Length, txtIPadd.Text, Convert.ToInt32(txtPort.Text)); //hint 3 of 3 in tips bubble (bytearray[] dgram, length of array, host name, port #);
+            udpClient.Close();
+        }
+    }
+}
