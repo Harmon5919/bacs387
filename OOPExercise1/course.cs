ada4334c60bb3d760401a0477fa28fb69161a6f2
Harmon5919 <57842300+Harmon5919@users.noreply.github.com>
2020-02-10 10:32:21 -0700

    merge oop1 and oop2

---
+++ OOPExercise1/course.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExercise1
+{
+    class Course
+    {
+        public Course(string courseName, int courseId, List<Student> studentList)
+        {
+            this.CourseName = courseName;
+            this.CourseId = courseId;
+            this.StudentList = studentList;
+        }
+        public string CourseName { get; set; }
+        public int CourseId { get; set; }
+        public List<Student> StudentList { get; set; }
+
+        public void GetAverageGrade()
+        {
+            decimal average = 0;
+
+        }
+
+        public void GetMin()
+        {
+            int min = 100;
+        }
+
+        public void GetMax()
+        {
+
+        }
+
+        public void GetPercent()
+        {
+
+        }
+    }
+}
