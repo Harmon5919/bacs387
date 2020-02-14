bd9d158cde95ffe1eb6b93c2f7f03e6aebbaf4ad
Harmon5919 <57842300+Harmon5919@users.noreply.github.com>
2020-02-14 09:55:12 -0700

    add error handling

--- BACS380ServerUDP/Form1.cs
+++ BACS380ServerUDP/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 namespace BACS380ServerUDP
 {
     public partial class frmServer : Form
@@ -17,22 +18,29 @@ namespace BACS380ServerUDP
         {
             InitializeComponent();
         }
-
-        private void btnStart_Click(object sender, EventArgs e)
+        private void Receiver()
         {
-            UdpClient udpClient = new UdpClient(1100); //add using system.net.sockets
+            UdpClient udpClient = new UdpClient(1100); //add using system.net.sockets //port must be specified on server side
             IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0); //add using system.net
             string receiveString;
             Byte[] receiveBytes = new byte[] { }; //array of bytes
 
             while (true)
             {
-                receiveBytes = udpClient.Receive(ref RemoteIpEndPoint); //waits for message, when received, assigns message to Byte container
+                receiveBytes = udpClient.Receive(ref RemoteIpEndPoint); //waits for message, when received, assigns message to Byte container, blocking call
                 receiveString = Encoding.ASCII.GetString(receiveBytes); //converts bytes to string
                 txtMessage.AppendText(receiveString);
                 txtMessage.AppendText(Environment.NewLine); //new line
                 txtMessage.Refresh(); //refreshes form manually to show any new text
             }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread receivingThread = new Thread(Receiver);
+            receivingThread.IsBackground = true;
+            receivingThread.Start();
+            btnStart.Enabled = false;
         }
     }
 }
